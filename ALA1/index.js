7bbf58fe6af8de555cb374ceb5d5150c0a279eb4
Mateo Galetto <mateogaletto13@gmail.com>
2026-09-06 20:40:14 -0300

    calculadora

---
+++ ALA1/index.js
@@ -0,0 +1,56 @@
+const readline = require("readline");
+// Creamos la interfaz de lectura
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+//---------------------------------
+
+rl.question("Ingrese una operacion -> '+', '-', '*', '/'\n-> ", (operacion) => {
+  switch (operacion) {
+    case "+":
+      rl.question("Ingrese un numero: ", (num1) => {
+        rl.question("Ingrese otro numero: ", (num2) => {
+          let res = Number(num1) + Number(num2);
+          console.log("El resultado es: ", res);
+          rl.close();
+        });
+      });
+      break;
+
+    case "-":
+      rl.question("Ingrese un numero: ", (num1) => {
+        rl.question("Ingrese otro numero: ", (num2) => {
+          let res = Number(num1) - Number(num2);
+          console.log("El resultado es: ", res);
+          rl.close();
+        });
+      });
+      break;
+
+    case "*":
+      rl.question("Ingrese un numero: ", (num1) => {
+        rl.question("Ingrese otro numero: ", (num2) => {
+          let res = Number(num1) * Number(num2);
+          console.log("El resultado es: ", res);
+          rl.close();
+        });
+      });
+      break;
+
+    case "/":
+      rl.question("Ingrese un numero: ", (num1) => {
+        rl.question("Ingrese otro numero: ", (num2) => {
+          let res = Number(num1) / Number(num2);
+          console.log("El resultado es: ", res);
+          rl.close();
+        });
+      });
+      break;
+
+    default:
+      console.log("Operacion invalida");
+
+      break;
+  }
+});
